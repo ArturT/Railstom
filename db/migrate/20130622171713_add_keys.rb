82cc306af7f9f94303a28badabcb63f291b7ce41
ArturT <arturtrzop@gmail.com>
2013-06-22 19:22:21 +0200

    Add missing foreign key user_id in authentications table

---
+++ db/migrate/20130622171713_add_keys.rb
@@ -0,0 +1,5 @@
+class AddKeys < ActiveRecord::Migration
+  def change
+    add_foreign_key "authentications", "users", :name => "authentications_user_id_fk"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130622151347) do
+ActiveRecord::Schema.define(:version => 20130622171713) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id",    :null => false
@@ -49,5 +49,7 @@ ActiveRecord::Schema.define(:version => 20130622151347) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
+
+  add_foreign_key "authentications", "users", :name => "authentications_user_id_fk"
 
 end
