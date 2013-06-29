7168d5a36d657e8ec7d5072f5f34d5d283ba7ad5
ArturT <arturtrzop@gmail.com>
2013-06-29 22:06:27 +0200

    Fix overwrite robots.txt task in staging deploy config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,7 +10,8 @@ set :deploy_to, "#{Figaro.env.capistrano_deploy_to}#{application}-#{rails_env}"
 namespace :deploy do
   desc "Overwrite robots.txt if we are not deploying to production environment"
   task :overwrite_robotstxt do
-    run "echo \"User-Agent: *\\nDisallow: /\" > #{release_path}/public/robots.txt"
+    run "echo 'User-Agent: *' > #{release_path}/public/robots.txt"
+    run "echo 'Disallow: /' >> #{release_path}/public/robots.txt"
   end
 end
 
