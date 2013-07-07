f00cb2bf28de5b6bab143fce25e8cc97e0f58387
ArturT <arturtrzop@gmail.com>
2013-07-07 17:59:48 +0200

    Add my account common partial and menu_items helper. Add link to my account in top nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,49 @@ module ApplicationHelper
       end
     end
   end
+
+  # Generate li elements for html list
+  #
+  # Params:
+  # @param *items [Array<Hash>] array of hashes
+  #   Params for each hash in above array:
+  #   @param name [String] link name
+  #   @param icon [String optional] awesome fonts class for icon i.e. icon-lock
+  #   @param path [String or Array<String, Hash>] URI for href, first element in Array is default path. Hash i.e. {controller: 'registrations', action: 'update'}
+  #   @param condition [Boolean optional] when true or nil link will be displayed
+  # @return [String]
+  def menu_items(*items)
+    items = items.map do |item|
+      if item[:condition] || item[:condition].nil?
+        menu_items = nil
+
+        if item[:path].kind_of?(Array)
+          paths = item[:path]
+        else
+          paths = [item[:path]]
+        end
+
+        class_name = nil
+        paths.each do |path|
+          if path.kind_of?(Hash)
+            class_name = 'active' if params[:controller] == path[:controller] and params[:action] == path[:action]
+          elsif current_page?(path)
+            class_name = 'active'
+          end
+        end
+
+        icon = item[:icon].nil? ? '' : %{<i class="#{item[:icon]}"></i>}
+        link_name = "#{icon} #{item[:name]}"
+        menu_items = %Q{<li class="#{class_name}">#{link_to raw(link_name), paths[0], method: item[:method]}</li>}
+      end
+
+      if item[:divider]
+        menu_items << %Q{<li class="divider"></li>}
+      end
+
+      menu_items
+    end
+
+    raw items.join
+  end
 end
