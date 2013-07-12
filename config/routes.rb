55153664d840901eb38cb83ee1b247018c0d6884
ArturT <arturtrzop@gmail.com>
2013-07-12 14:16:04 +0200

    Fix routes for active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Railstom::Application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
 
   root :to => 'home#locale_root'
 
@@ -10,8 +8,10 @@ Railstom::Application.routes.draw do
   scope '/:locale', constraints: { locale: /[a-z]{2}/ } do
     root :to => 'home#index'
 
+    devise_for :admin_users, ActiveAdmin::Devise.config
+    ActiveAdmin.routes(self)
+
     devise_for :users, controllers: {
-  ActiveAdmin.routes(self)
       omniauth_callbacks: 'omniauth_callbacks',
       registrations: 'registrations'
     }
