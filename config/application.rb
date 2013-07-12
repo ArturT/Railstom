6f5a2c4229371456e342870ec448500bcc7199d3
ArturT <arturtrzop@gmail.com>
2013-07-12 19:38:49 +0200

    Add ext and hacks directories to lib. Add them to autoload paths

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Railstom
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib/autoload)
+    config.autoload_paths += %W(#{config.root}/lib/autoload #{config.root}/lib/ext #{config.root}/lib/hacks)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
