ed318f843d0f2998447be68edbe89e9510644fe9
ArturT <arturtrzop@gmail.com>
2013-07-13 22:03:44 +0200

    Login as confirmed user in controller macros

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -10,8 +10,7 @@ module ControllerMacros
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryGirl.create(:user_unconfirmed)
-      user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the confirmable module
+      user = FactoryGirl.create(:user)
       sign_in user
     end
   end
