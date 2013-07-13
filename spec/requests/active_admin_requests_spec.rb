e9027b15800001d0555ced9001833d0dbaafc7af
ArturT <arturtrzop@gmail.com>
2013-07-13 21:46:19 +0200

    Refactoring in active admin requests spec. Add case for logged in user

--- spec/requests/active_admin_requests_spec.rb
+++ spec/requests/active_admin_requests_spec.rb
@@ -2,23 +2,33 @@ require 'spec_helper'
 
 describe 'Active Admin Requests' do
   let(:admin) { create(:admin) }
+  let(:user) { create(:user_confirmed) }
 
   subject { response }
 
   describe '#admin_root_path' do
-    context 'when user is admin' do
-      before do
-        login_as(admin)
-        get admin_root_path
+    context 'when user is logged in' do
+      context 'when user is admin' do
+        before do
+          login_as(admin)
+          get admin_root_path
+        end
+
+        it { should be_success }
       end
 
-      it { should be_success }
+      context 'when user is not admin' do
+        before do
+          login_as(user)
+          get admin_root_path
+        end
+
+        it { should redirect_to root_path }
+      end
     end
 
-    context 'when user is not admin' do
-      before do
-        get admin_root_path
-      end
+    context 'when user is logged out' do
+      before { get admin_root_path }
 
       it { should redirect_to root_path }
     end
