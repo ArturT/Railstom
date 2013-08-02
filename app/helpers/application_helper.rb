a619e044ca612a2fcec51ce5874e7fc50cfc0fb2
ArturT <arturtrzop@gmail.com>
2013-08-02 22:44:45 +0200

    Add better colors for alert flash notice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
   end
 
   def alert_box(css_class, message)
-    content_tag :div, class: "alert-box #{css_class}", data: { alert: nil } do
+    content_tag :div, class: "alert-box alert-#{css_class}", data: { alert: nil } do
       content_tag :div, class: 'row' do
         raw(%{#{message} <a href="#" class="close">&times;</a>})
       end
