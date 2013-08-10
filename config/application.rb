9681b24600576cf6a0233b847ab6ce65529257c4
ArturT <arturtrzop@gmail.com>
2013-08-10 18:18:55 +0200

    Enable assets.initialize_on_precompile to precompile i18n-js translations

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,9 @@ module Railstom
     # Enable the asset pipeline
     # config.assets.enabled = true
 
+    # Enable to precompile i18n-js translations
+    config.assets.initialize_on_precompile = true
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
