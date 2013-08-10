1f5fd3cf09a3f867cf4efc189a45bf5a7994a649
ArturT <arturtrzop@gmail.com>
2013-08-10 19:24:08 +0200

    Fix templates controller spec

--- spec/controllers/templates_controller_spec.rb
+++ spec/controllers/templates_controller_spec.rb
@@ -4,11 +4,13 @@ describe TemplatesController do
   subject { response }
 
   describe '#show' do
+    let(:template_id) { 'test' }
+
     before do
-      get :show, id: 'test'
+      get :show, id: template_id
     end
 
     it { should be_success }
-    it { should render_template(page) }
+    it { should render_template(template_id) }
   end
 end
