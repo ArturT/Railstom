6645055037d0e13e4e4ef9e30f34d3ac9a366df2
ArturT <arturtrzop@gmail.com>
2013-09-01 13:58:40 +0200

    Use gravatar as default url for fallback avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -23,12 +23,15 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    email_address = model.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    unless version_name.nil?
+      size = AvatarUploader.const_get("#{version_name.upcase}_WIDTH")
+      size_param = "?s=#{size}"
+    end
+    "http://www.gravatar.com/avatar/#{hash}#{size_param}"
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
