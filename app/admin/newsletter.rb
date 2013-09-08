c19c66bcde778789c00e310a830e6ce8ae2c707c
ArturT <arturtrzop@gmail.com>
2013-09-08 13:45:41 +0200

    Add newsletter to admin

---
+++ app/admin/newsletter.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register Newsletter do
+  index do
+    column :id
+    column :title
+    column :enabled_force
+    column :stopped
+    column :started_at
+    column :finished_at
+    column :created_at
+    column :updated_at
+
+    default_actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :body
+      f.input :enabled_force
+      f.input :stopped
+      f.input :started_at
+    end
+    f.actions
+  end
+end
