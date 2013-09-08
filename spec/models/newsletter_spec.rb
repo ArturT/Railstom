c5065bb77c138d331c3ce9135ed8534c4391252c
ArturT <arturtrzop@gmail.com>
2013-09-09 00:38:53 +0200

    Add spec for send_preview_email validate in newsletter model

--- spec/models/newsletter_spec.rb
+++ spec/models/newsletter_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe Newsletter do
-  subject { build(:newsletter) }
+  let(:newsletter) { build(:newsletter) }
+
+  subject { newsletter }
 
   it { should be_valid }
   it { should validate_presence_of(:subject) }
@@ -9,4 +11,37 @@ describe Newsletter do
   its(:enabled_force) { should be_false }
   its(:stopped) { should be_false }
   its(:last_user_id) { should eql 0 }
+  its(:preview_email) { should be_blank }
+
+  describe 'private methods' do
+    describe '#send_preview_email' do
+      before do
+        subject.preview_email = email
+        subject.valid?
+      end
+
+      context 'when preview email is blank' do
+        let(:email) { '' }
+
+        it 'has no error on preview_email' do
+          expect(subject.errors).to have(0).error_on(:preview_email)
+        end
+      end
+
+      context 'when preview email is not blank', :sidekiq_inline do
+        let(:email) { 'email@example.com' }
+        let(:mail) { ActionMailer::Base.deliveries.last }
+
+        it 'has error on preview email' do
+          expect(subject.errors).to have(1).error_on(:preview_email)
+        end
+
+        describe 'delivers newsletter to preview email' do
+          its(:to) { expect(mail.to).to include email }
+          its(:subject) { expect(mail.subject).to eql newsletter.subject }
+          its(:body) { expect(mail.body.to_s).to include newsletter.body }
+        end
+      end
+    end
+  end
 end
