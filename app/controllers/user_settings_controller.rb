c81e02a757a53c4fad7cf3e831004ebf1ef0f84f
ArturT <arturtrzop@gmail.com>
2013-09-08 19:14:43 +0200

    User can edit preferred language for email notification

--- app/controllers/user_settings_controller.rb
+++ app/controllers/user_settings_controller.rb
@@ -16,6 +16,6 @@ class UserSettingsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:avatar, :avatar_cache, :remove_avatar, :enabled_newsletter)
+    params.require(:user).permit(:avatar, :avatar_cache, :remove_avatar, :enabled_newsletter, :preferred_language)
   end
 end
