155ac230dca7d92bb794d3306e5f386f36eadb23
ArturT <arturtrzop@gmail.com>
2013-09-09 01:41:33 +0200

    Add newsletter spec for NewsletterMailer

--- spec/mailers/newsletter_mailer_spec.rb
+++ spec/mailers/newsletter_mailer_spec.rb
@@ -1,5 +1,22 @@
 require "spec_helper"
 
 describe NewsletterMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  describe '.newsletter' do
+    let(:user) { build(:user) }
+    let(:newsletter) { build(:newsletter) }
+
+    subject { ActionMailer::Base.deliveries.last }
+
+    before do
+      NewsletterMailer.newsletter(user, newsletter.subject, newsletter.body).deliver
+    end
+
+    its(:to) { should include user.email }
+    its(:subject) { should eql newsletter.subject }
+    its(:body) { should include newsletter.body }
+  end
 end
