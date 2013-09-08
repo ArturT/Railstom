3711aa30d2ad101f6e8d671cf421a3942f04923a
ArturT <arturtrzop@gmail.com>
2013-09-08 12:26:35 +0200

    Improve create newsletter migration

--- db/migrate/20130908101354_create_newsletters.rb
+++ db/migrate/20130908101354_create_newsletters.rb
@@ -1,11 +1,11 @@
 class CreateNewsletters < ActiveRecord::Migration
   def change
     create_table :newsletters do |t|
-      t.string :title
-      t.text :body
-      t.boolean :enabled_force
-      t.boolean :stopped
-      t.integer :last_user_id
+      t.string :title, null: false
+      t.text :body, null: false
+      t.boolean :enabled_force, null: false, default: false
+      t.boolean :stopped, null: false, default: false
+      t.integer :last_user_id, null: false, default: 0
       t.datetime :started_at
       t.datetime :finished_at
 
