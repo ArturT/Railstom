5260dbb9676b504ca08bcc10fa5d420b1b3d91ca
ArturT <arturtrzop@gmail.com>
2013-09-15 21:05:22 +0200

    Use let in injector

--- app/services/injector.rb
+++ app/services/injector.rb
@@ -8,17 +8,9 @@ class Injector
     @request = request
   end
 
-  def current_user
-    @current_user
-  end
-
-  def request
-    @request
-  end
-
-  def omniauth_hash
-    request.env['omniauth.auth']
-  end
+  let(:current_user) { @current_user }
+  let(:request) { @request }
+  let(:omniauth_hash) { request.env['omniauth.auth'] }
 
   %(User Authentication).each do |klass|
     define_method "db_#{klass.name.underscore}" do
