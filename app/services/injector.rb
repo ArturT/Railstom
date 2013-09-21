4e8e647cd857d8529193ed2df5a6e356a016cb35
ArturT <arturtrzop@gmail.com>
2013-09-22 00:54:21 +0200

    Fix injector one more time

--- app/services/injector.rb
+++ app/services/injector.rb
@@ -1,11 +1,14 @@
 class Injector
   include Dependor::AutoInject
+  extend Dependor::Let
 
   def initialize(current_user = nil, request = nil)
     @current_user = current_user
     @request = request
   end
 
+  let(:current_user) { @current_user }
+  let(:request) { @request }
   let(:omniauth_hash) { @request.env['omniauth.auth'] }
 
   %w(User Authentication).each do |klass|
