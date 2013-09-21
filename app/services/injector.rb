44321468e20f46a42ecd9c92863397537078cc65
ArturT <arturtrzop@gmail.com>
2013-09-21 23:47:09 +0200

    Remove methods causes stack level to deep

--- app/services/injector.rb
+++ app/services/injector.rb
@@ -1,17 +1,12 @@
-require 'dependor/shorty'
-
 class Injector
   include Dependor::AutoInject
-  extend Dependor::Let
 
   def initialize(current_user = nil, request = nil)
     @current_user = current_user
     @request = request
   end
 
-  let(:current_user) { @current_user }
-  let(:request) { @request }
-  let(:omniauth_hash) { request.env['omniauth.auth'] }
+  let(:omniauth_hash) { @request.env['omniauth.auth'] }
 
   %w(User Authentication).each do |klass|
     define_method "db_#{klass.underscore}" do
