35dc0a3ed17fc7352baa5415d0bf6168a6ba050e
ArturT <arturtrzop@gmail.com>
2013-09-22 11:35:31 +0200

    Add spec for user repository find_with_omniauth

--- spec/repositories/user_repository_spec.rb
+++ spec/repositories/user_repository_spec.rb
@@ -2,12 +2,27 @@ require 'spec_helper'
 
 describe UserRepository do
   let(:db_user) { User }
-  let(:omniauth_hash) { Hash.new }
+  let(:user) { build(:user) }
+  let(:email) { 'email@example.com' }
+  let(:omniauth_hash) do
+    {
+      'info' => {
+        'email' => email
+      }
+    }
+  end
 
   subject { isolate(UserRepository) }
 
   describe '#build' do
     its(:build) { should be_a(User)}
     its(:build) { should_not be_nil }
+  end
+
+  describe '#find_with_omniauth' do
+    it 'returns user ' do
+      expect(db_user).to receive(:find_by).with(email: email).and_return(user)
+      expect(subject.find_with_omniauth).to eql user
+    end
   end
 end
