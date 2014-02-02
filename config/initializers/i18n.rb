1be907a78359de5e99731b66026cdd31c3ba49be
ArturT <arturtrzop@gmail.com>
2014-02-02 15:08:32 +0100

    Set I18n.enforce_available_locales to false [fix deprecated warning]

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -8,3 +8,7 @@ module I18n
   end
 
 end
+
+# Skip message:
+# [deprecated] I18n.enforce_available_locales will default to true in the future. If you really want to skip validation of your locale you can set I18n.enforce_available_locales = false to avoid this message.
+I18n.enforce_available_locales = false
