9bf51ecd679850bcc3a487fce9b050f49fcf858b
ArturT <arturtrzop@gmail.com>
2014-03-30 14:59:47 +0200

    #1305223 Change slim into haml views

--- spec/views/home/index.html.haml_spec.rb
+++ spec/views/home/index.html.haml_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'home/index.html.slim' do
+describe 'home/index.html.haml' do
   it 'view has content Hello World', :railstom do
     render
     expect(rendered).to have_content('Hello World')
